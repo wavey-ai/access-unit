f54263fee11452be759a30bdd1b8b8759fcbe74c
jbrough <jamie1612@gmail.com>
2025-02-09 20:35:42 +0000

    add idx

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,16 +1,21 @@
 pub struct LpChunkIter<'a> {
-    data: &'a [u8],
-    pos: usize,
+    pub data: &'a [u8],
+    pub pos: usize,
+    pub index: usize,
 }
 
 impl<'a> LpChunkIter<'a> {
     pub fn new(data: &'a [u8]) -> Self {
-        Self { data, pos: 0 }
+        Self {
+            data,
+            pos: 0,
+            index: 0,
+        }
     }
 }
 
 impl<'a> Iterator for LpChunkIter<'a> {
-    type Item = Result<&'a [u8], &'static str>;
+    type Item = Result<(usize, &'a [u8]), &'static str>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.pos + 4 > self.data.len() {
@@ -30,6 +35,8 @@ impl<'a> Iterator for LpChunkIter<'a> {
 
         let chunk = &self.data[self.pos..self.pos + chunk_len];
         self.pos += chunk_len;
-        Some(Ok(chunk))
+        let current_index = self.index;
+        self.index += 1;
+        Some(Ok((current_index, chunk)))
     }
 }
